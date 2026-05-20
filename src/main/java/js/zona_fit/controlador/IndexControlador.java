36eb9159ac245c2521d4cbe367ef9930a548827b
enriquea.suarez <enriquea.suarez@inetum.com>
2026-05-20 16:16:11 +0200

    corregir error

--- src/main/java/js/zona_fit/controlador/IndexControlador.java
+++ src/main/java/js/zona_fit/controlador/IndexControlador.java
@@ -1,34 +1,31 @@
 package js.zona_fit.controlador;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import js.zona_fit.modelo.Cliente;
 import js.zona_fit.servicio.IClienteServicio;
-import lombok.Getter;
-import lombok.Setter;
 
 @Component("indexControlador")
-@Scope("view")
-@Getter
-@Setter
+@Scope(value = "view", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class IndexControlador implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
 
 	@Autowired
 	private IClienteServicio clienteServicio;
 
-	private List<Cliente> clientes;
-
-	private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+	private List<Cliente> clientes = new ArrayList<>();
 
 	@PostConstruct
 	public void init() {
@@ -37,6 +34,16 @@ public class IndexControlador implements Serializable {
 
 	public void cargarDatos() {
 		this.clientes = this.clienteServicio.listarClientes();
-		this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+		if (this.clientes != null) {
+			this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+		}
+	}
+
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
 	}
 }
